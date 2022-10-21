08463d72778bbe03a9d4fd963e628723a023d029
Dan Sinensky <dan.i.sinensky@gmail.com>
2022-10-21 17:26:11 -0400

    Started project

---
+++ app.js
@@ -0,0 +1,63 @@
+// variable for api key
+const apiKey = "7d01835818934a4f8af21b99bad948ea"
+
+// variable for base url
+const baseURL = "https://newsapi.org/"
+
+// variable for date
+
+const year = new Date().getFullYear()
+const month = new Date().getMonth()
+const day = new Date().getDate()
+const date = `${year}-${month}-${day}`
+
+// function that does movie search
+function newsSearch(topic){
+    // constructing url for request
+    // Basic URL for stories about a topic from the last month
+    const url = `${baseURL}v2/everything?q=${topic}&from=${date}&sortby=publishedAt&apikey=${apiKey}`
+    // const url = `${baseURL}v2/everything?q=${title}&from=${date}&sortby=${sort}&apikey=${apiKey}`
+    
+    // All articles mentioning Apple from yesterday, sorted by popular publishers first
+    // https://newsapi.org/v2/everything?q=apple&from=2022-10-20&to=2022-10-20&sortBy=popularity&apiKey=API_KEY
+
+    // All articles about Tesla from the last month, sorted by recent first
+    // https://newsapi.org/v2/everything?q=tesla&from=2022-09-21&sortBy=publishedAt&apiKey=API_KEY
+
+    // Top business articles in the US right now
+    // https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=API_KEY
+
+    // Top articles from TechCrunch right now
+    // https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=API_KEY
+
+    // All articles published by the Wall Street Journal in the last 6 months, sorted by recent first
+    // https://newsapi.org/v2/everything?domains=wsj.com&apiKey=API_KEY
+
+    $.ajax(url)
+    .then((data) => {
+        console.log(data)
+        news = data
+        //render()
+        },
+        (error) => {
+            console.log('bad request', error)
+        }
+    )    
+
+
+}
+
+// //grab the submit button
+// $("input[type=submit]").on("click", (event) => {
+
+//     // prevent the refresh
+//     event.preventDefault()
+
+//     // grab text from input box
+//     const inputText = $("input[type=text]").val()
+    
+//     // update the screen
+//     newsSearch(inputText)
+// })
+
+newsSearch("Tesla")
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-# The Name of My App
+# The Aggregate Press Informer
 
-A description of my project
+A news application that creates customizable front pages by topic/date/reporter/outlet, saves them in localstorage, and links to the full articles for later reading
 
 ### Technologies Used
 
@@ -8,21 +8,25 @@ A description of my project
 - CSS
 - JavaScript
 - jQuery
-- API source 1
-- API source 2
-- API source 3
+- Google Fonts
+- API
+  - News API
   
 ### Wireframes
 
-![Some text](https://i.ytimg.com/vi/mRf3-JkwqfU/mqdefault.jpg)
+![My Wireframe](https://i.ytimg.com/vi/mRf3-JkwqfU/mqdefault.jpg)
 
 ### Daily Plan
 
 | Day | Goal |
 |-----|------|
-| 1 | Choose API |
-| 2 | Render data to screen |
-| 3 | Create form |
+| 1 - Saturday | Finalize API, README.md, wireframe. Create working AJAX callback and event listener to change stories |
+| 2 - Sunday | Create DOM manipulation with AJAX to filter stories |
+| 3 - Monday | Create DOM manipulation to customize where stories are on page |
+| 4 - Tuesday | Localstorage to save pages |
+| 5 - Wednesday | Create carousel of stored pages |
+| 6 - Thursday | Create nav bar, move filter to nav bar, improve filter |
+| 7 - Friday | CSS and final touches/catch-up |
 
 ### Link to Project
 [My Project Live Page](https://dansinensky.github.io/turmeric-project1/)
